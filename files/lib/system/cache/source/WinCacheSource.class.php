cc3e1028c5d45c9e09d18c666b2fa3448f494eb3
Stricted <info@stricted.de>
2014-01-21 15:53:10 +0100

    add beta 1 files

---
+++ files/lib/system/cache/source/WinCacheSource.class.php
@@ -0,0 +1,113 @@
+<?php
+namespace wcf\system\cache\source;
+use wcf\system\exception\SystemException;
+use wcf\system\Regex;
+use wcf\util\StringUtil;
+
+/**
+ * WinCacheSource is an implementation of CacheSource that uses wincache for windows to store data.
+ *
+ * @author      Jan Altensen (Stricted)
+ * @copyright   2013-2014 Jan Altensen (Stricted)
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     be.bastelstu.jan.wcf.wincache
+ * @category    Community Framework
+ */
+class WinCacheSource implements ICacheSource {
+	/**
+	 * key prefix
+	 * @var	string
+	 */
+	protected $prefix = '';
+	
+	/**
+	 * Creates a new WinCacheSource object.
+	 */
+	public function __construct() {
+		if (!extension_loaded('wincache')) {
+			throw new SystemException('The php extension "wincache" is not loaded');
+			exit;
+		}
+		
+		// set variable prefix to prevent collision
+		$this->prefix = 'WCF_'.substr(sha1(WCF_DIR), 0, 10) . '_';
+	}
+	
+	/**
+	 * Flushes a specific cache, optionally removing caches which share the same name.
+	 * 
+	 * @param	string		$cacheName
+	 * @param	boolean		$useWildcard
+	 */
+	public function flush($cacheName, $useWildcard) {
+		if ($useWildcard) $this->removeKeys($this->prefix . $cacheName . '(\-[a-f0-9]+)?');
+		else wincache_ucache_delete($this->prefix . $cacheName);
+	}
+	
+	/**
+	 * Clears the cache completely.
+	 */
+	public function flushAll() {
+		$this->removeKeys();
+	}
+	
+	/**
+	 * Returns a cached variable.
+	 * 
+	 * @param	string		$cacheName
+	 * @param	integer		$maxLifetime
+	 * @return	mixed
+	 */
+	public function get($cacheName, $maxLifetime) {
+		if (!wincache_ucache_exists($cacheName)) return null;
+		return wincache_ucache_get($cacheName);
+	}
+	
+	/**
+	 * Stores a variable in the cache.
+	 * 
+	 * @param	string		$cacheName
+	 * @param	mixed		$value
+	 * @param	integer		$maxLifetime
+	 */
+	public function set($cacheName, $value, $maxLifetime) {
+		wincache_ucache_set($cacheName, $value, $this->getTTL($maxLifetime));
+	}
+	
+	/**
+	 * Returns time to live in seconds, defaults to 3 days.
+	 * 
+	 * @param	integer		$maxLifetime
+	 * @return	integer
+	 */
+	protected function getTTL($maxLifetime = 0) {
+		if ($maxLifetime && ($maxLifetime <= (60 * 60 * 24 * 30) || $maxLifetime >= TIME_NOW)) {
+			return $maxLifetime;
+		}
+		
+		// default TTL: 3 days
+		return (60 * 60 * 24 * 3);
+	}
+	
+	/**
+	 * remove cache data
+	 *
+	 * @param	string	$pattern	<optional>
+	 */
+	public function removeKeys($pattern = null) {
+		$regex = null;
+		if ($pattern !== null) $regex = new Regex('^'.$pattern.'$');
+		
+		$apcCacheInfo = APC::cache_info();
+		foreach ($apcCacheInfo as $cache) {
+			if ($regex === null) {
+				if (StringUtil::startsWith(basename($cache['key_name']), $this->prefix)) {
+					wincache_ucache_delete(basename($cache['key_name']));
+				}
+			}
+			else if ($regex->match($cache['info'])) {
+				wincache_ucache_delete(basename($cache['key_name']));
+			}
+		}
+	}
+}
---
+++ files/lib/system/event/listener/WinCacheListener.class.php
@@ -0,0 +1,78 @@
+<?php
+namespace wcf\system\event\listener;
+use wcf\system\event\IEventListener;
+use wcf\system\exception\SystemException;
+use wcf\system\Regex;
+use wcf\system\WCF;
+
+/**
+ * @author      Jan Altensen (Stricted)
+ * @copyright   2013-2014 Jan Altensen (Stricted)
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     be.bastelstu.jan.wcf.wincache
+ * @category    Community Framework
+ */
+class WinCacheListener implements IEventListener {
+	/**
+	 * @see \wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		switch ($className) {
+			case "wcf\acp\page\CacheListPage":
+				if ($eventObj->cacheData['source'] == 'wcf\system\cache\source\WinCacheSource') {
+					
+					// set version
+					$eventObj->cacheData['version'] = phpversion('wincache');
+					
+					$prefix = new Regex('^WCF_'.substr(sha1(WCF_DIR), 0, 10) . '_');
+					$data = array();
+					foreach (wincache_ucache_info() as $cache) {
+						if (!$prefix->match(basename($cache['key_name']))) continue;
+						
+						// get additional cache information
+						$data['data']['WinCache'][] = array(
+							'filename' => $prefix->replace(basename($cache['key_name']), ''),
+							'filesize' => $cache['value_size'],
+							'mtime' => TIME_NOW - $cache['age_seconds']
+						);
+						$eventObj->cacheData['files']++;
+						$this->cacheData['size'] += $cache['value_size'];
+					}
+					$eventObj->caches = array_merge($data, $eventObj->caches);
+				}
+				break;
+			
+			case "wcf\system\option\OptionHandler":
+				$eventObj->cachedOptions['cache_source_type']->modifySelectOptions($eventObj->cachedOptions['cache_source_type']->selectOptions . "\nwin:wcf.acp.option.cache_source_type.wincache");
+				
+				/* dirty but i need wait for pull request https://github.com/WoltLab/WCF/pull/1630 */
+				$eventObj->cachedOptions['cache_source_type']->enableOptions = $eventObj->cachedOptions['cache_source_type']->enableOptions . "\nwin:!cache_source_memcached_host";
+				break;
+			
+			case "wcf\acp\action\UninstallPackageAction":
+				$packageID = 0;
+				if (isset($_POST['packageID']) && !empty($_POST['packageID'])) $packageID = intval($_POST['packageID']);
+				
+				if ($packageID) {
+					$sql = "SELECT * FROM wcf".WCF_N."_package where package = ? LIMIT 1";
+					$statement = WCF::getDB()->prepareStatement($sql);
+					$statement->execute(array("be.bastelstu.jan.wcf.wincache"));
+					$row = $statement->fetchArray();
+					if ($packageID == $row['packageID']) {
+						// set cache to disk if apc(u) is enabled
+						$sql = "UPDATE	wcf".WCF_N."_option
+							SET	optionValue = ?
+							WHERE	optionName = ?
+								AND optionValue = ?";
+						$statement = WCF::getDB()->prepareStatement($sql);
+						$statement->execute(array(
+							'disk',
+							'cache_source_type',
+							'win'
+						));
+					}
+				}
+				break;
+		}
+	}
+}
